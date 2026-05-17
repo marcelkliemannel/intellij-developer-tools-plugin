0a6d7ec334133148957998250ef5c047e334d09c
Marcel Kliemannel <dev@marcelkliemannel.com>
2026-05-17 19:52:09 +0200

    fix: remove closed workbenches from restored tools

--- modules/tools/ui/src/main/kotlin/dev/turingcomplete/intellijdevelopertoolsplugin/tool/ui/frame/menu/DeveloperToolNode.kt
+++ modules/tools/ui/src/main/kotlin/dev/turingcomplete/intellijdevelopertoolsplugin/tool/ui/frame/menu/DeveloperToolNode.kt
@@ -51,6 +51,7 @@ class DeveloperToolNode(
           developerToolId = developerToolId,
           developerToolConfiguration = it.configuration,
         )
+        _developerTools.remove(it)
         Disposer.dispose(developerUiTool)
       }
   }
